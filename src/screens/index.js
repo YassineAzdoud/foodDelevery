1bb56fc059bd155ac56cb96803829a59736f1d4e
YassineAzdoud <yassinelife2030@gmail.com>
2022-05-16 14:44:10 +0100

    add verification screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,19 @@
-import SplachScreen from "./SplachScreen";
-import WelcomeScreen from "./WelcomeScreen";
-import SigninScreen from "./SigninScreen";
-import SignupScreen from "./SignupScreen"
-import ForgotPasswordScreen from "./ForgotPasswordScreen"
-import RegisterPhoneScreen from "./RegisterPhoneScreen"
+import SplashScreen from './SplashScreen';
+import WelcomeScreen from './WelcomeScreen';
+import SigninScreen from './SigninScreen';
+import SignupScreen from './SignupScreen';
+import ForgotPasswordScreen from './ForgotPasswordScreen';
+import RegisterPhoneScreen from './RegisterPhoneScreen';
+import VerificationScreen from './VerificationScreen';
+import HomeScreen from './HomeScreen';
 
-export {SplachScreen, WelcomeScreen, SigninScreen, SignupScreen, ForgotPasswordScreen, RegisterPhoneScreen};
+export {
+  SplashScreen,
+  WelcomeScreen,
+  SigninScreen,
+  SignupScreen,
+  ForgotPasswordScreen,
+  RegisterPhoneScreen,
+  VerificationScreen,
+  HomeScreen,
+};
